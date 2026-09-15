3ea0e57488b39af3f9505b65c25354d1362ce9ed
xMicte2200 <angelesmundo131@gmail.com>
2026-09-15 17:19:43 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = "https://bkukgzyvoqnwqaxxwpua.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_y_XUnQ391GTLQCPwgZunuA_Pen_NoXZ";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
